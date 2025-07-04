67b0d3b5b97bade01c1fba3d38350bf274df1b19
aborruso <aborruso@gmail.com>
2025-07-04 17:35:17 +0200

    chore: update tailwindcss version and add tailwind configuration file
    - Removed the dependency on @tailwindcss/vite.
    - Updated tailwindcss to version 3.4.17.
    - Added a new tailwind.config.mjs file for Tailwind CSS configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,18 +2,14 @@
 import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
-import tailwindcss from '@tailwindcss/vite';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), tailwind()],
 
   // Configurazione per GitHub Pages
   site: 'https://aborruso.github.io', // Sostituisci con il tuo username GitHub
   base: '/referendum_astro', // Nome del repository
   output: 'static', // Genera sito statico
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
 });
